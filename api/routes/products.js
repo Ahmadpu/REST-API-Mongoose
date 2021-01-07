9e58073b6355399f463cfc7e3ccd22e06abe356b
Ahmadpu <ahmad1.ngxoft@gmail.com>
2021-01-07 19:23:09 +0500

    Add Controller

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,10 +1,11 @@
 
     const express = require('express');
     const router = express.Router();
-    const mongoose = require('mongoose');
-    const product = require('../models/product');
+    const Product = require('../models/product');
     const multer = require('multer');
     const path = require('path');
+    const checkAuth = require('../middleware/checkAuth');
+    const productsController = require('../controllers/products');
     const storage = multer.diskStorage({
         destination: function(req,file,cb){
             cb(null, 'uploads')
@@ -26,142 +27,20 @@
         fileSize : 1024 * 1024 * 5 
     }});
 
-    const Product = require('../models/product');
+    
 
     // router.get('/about',(req,res,next) => {
     //     res.status(200).json({
     //         message: 'handling GET requests to /products/about'
     //     })
     // });
-    router.get('/',(req,res,next) => {
-        Product.find()
-            .select("name price _id productImage")
-            .exec()
-            .then(docs => {
-                const response = {
-                    count : docs.length,
-                    products: docs.map(doc => {
-                        return{
-                            name : doc.name,
-                            price : doc.price,
-                            productImage : doc.productImage,
-                            _id : doc._id,
-                            request:{
-                                type : 'GET',
-                                url : 'http://localhost:3000/products/' + doc._id
-                            }
-                        }
-                    })
-                }
-                // console.log(docs);
-                // if(docs.length >= 0){
-                    res.status(200).json(response);
-                // }else{
-                //     res.status(404).json({
-                //         message : 'No Entries found'
-                //     })
-                // }
-        //     res.status(200).json(docs);
-          })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error : err
-                })
-        });
-    //     res.status(200).json({
-    //     message: 'handling GET requests to /products/home'
-    //     })
-    });
-    router.get('/products',(req,res,next)=>{
-        Product.products()
-        
-    })
+    router.get('/',productsController.products_get);
 
-    router.post('/',upload.single('productImage'),(req,res,next) => {
-       
-        const product = new Product({
-            _id : new mongoose.Types.ObjectId(),
-            name: req.body.name,
-            price: req.body.price,
-            productImage : req.file.path
-        });
-        product
-            .save()
-            .then(result =>{
-                console.log(result);
-                res.status(201).json
-                    {
-                    message : "created object successfully"
-                    createdProduct : {
-                        name : result.name
-                        price : result.price
-                        _id : result._id
-                        request:{
-                            type : 'POST'
-                            url : 'http://localhost:3000/products/' + doc._id
-                        }
-                    } 
-                }
-            })
-            .catch(err => console.log(err));
-        res.status(201).json({
-            message : 'Handling Post request to /product',
-            createdProduct : {product}
-        });
-    });
-    router.get('/:productId',(req,res,next)=> {
-        const id=req.params.productId;
-        Product.findById(id)
-        .select( 'name price _id productImage').exec()
-        .then(doc => { 
-            console.log("From database", doc);
-            if(doc){
-                res.status(200).json({
-                    product : doc,
-                    request : {
-                        type : 'GET',
-                        description : 'GET all products',
-                        url : 'http://localhost:3000/products'
-                    }
-                });
-            }else{
-                res.status(404).json({message : 'No Valid entry found for provided ID'})
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({error : err})
-        });
-    });
-    router.patch('/:productId',(req,res,next) => {
-        const id= req.params.productId;
-        const updateOps = {};
-        // for(const ops in req.body){
-            // updateOps[ops.propName] = ops.value;
-        // }
-        updateOps[req.body.propName] = req.body.value;
-        console.log("updateOps",updateOps);
-        Product.update({ _id : id},{$set: updateOps })    
-        .exec()
-        .then(result =>{
-            res.status(200).json({
-                message : "Product Updated",
-                request: {
-                    type : 'GET',
-                    url : 'http://localhost:3000/products/'+ id
-                }
-            }); 
-        }).catch(err=> {
-            res.status(500).json({
-                err : err
-            });
-        });
-    });
-    router.delete('/:productId',(req,res,next)=>{
-        const id= req.params.productId;
-        Product.remove({_id : id});
-    })
+
+    router.post('/', checkAuth, upload.single('productImage'),productsController.products_post);
+    router.get('/:productId',);
+    router.patch('/:productId',checkAuth, productsController.products_update);
+    router.delete('/:productId',checkAuth,productsController.products_delete);
     
     // router.delete();
     // router.post('/:productId',(req,res,next) => {
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
@@ -9,9 +10,15 @@ const productsRoutes = require('./api/routes/products')
 const ordersRoutes = require('./api/routes/orders');
 const userRoutes = require('./api/routes/user');
 
-//MongoDB configuration
-const dbConfig = require('../api/api/dbconfig');
+//MongoDB configuration of REST API
+const dbConfig = require('./api/config/dbconfig');
+//MongoDB configuration of CRUD
+
+//const dbConfigCRUD = require('./api/config/database.config')
+//require global mongoose
 const mongoose = require('mongoose');
+
+
 
 mongoose.Promise =global.Promise;
 // MongoDB connection
@@ -19,7 +26,7 @@ mongoose.Promise =global.Promise;
                     // mongoose.connect('mongodb+srv://node-rest:<node-rest>@node-rest-shop.b6c66.mongodb.net/<node-rest-shop>?retryWrites=true&w=majority',
                     // {  useNewUrlParser: true }
                     // )
-
+//connecting the database REST_api 
 mongoose.connect(dbConfig.url,{
     useNewUrlParser : true
 }).then(()=>{
@@ -28,15 +35,26 @@ mongoose.connect(dbConfig.url,{
         console.log("db not connected due to some error",err);
         process.exit();
 })
-
+// connecting the database CRUD
+// mongoose.connect(dbConfigCRUD.url,{
+//     useNewUrlParser : true
+// }).then(()=>{
+//     console.log('Successfully connected to the database');
+// }).catch(err=>{
+//     console.log('Could not connect to the database ...',err);
+//     process.exit();
+// })
 
 
 //Morgan third-party middleware
 app.use(morgan('dev'));
 app.use('./uploads', express.static(path.join(__dirname, './uploads')));
 
-// bodyparser 
+            // bodyparser 
+//parse requests of content -type-application/json
+app.use(bodyParser.json());
 
+//  parse requests of content-type-application
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
@@ -45,9 +63,7 @@ app.use(express.json());
 app.use((req,res,next) =>{
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Headers', 'Origin,X-Requested with, Content-Type, Accept, Authorization',
-    {
-        
-    });
+    );
     if(req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
         return res.status(200).json({});
@@ -59,6 +75,9 @@ app.use((req,res,next) =>{
 app.use('/products' , productsRoutes);
 app.use('/orders' , ordersRoutes);
 app.use('/user', userRoutes);
+
+//require Notes routes
+require('../CRUD&REST-api/api/routes/note.routes')(app);
 
 // Handling Error
 app.use((req,res,next) => {
